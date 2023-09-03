cc5e644e2fc1599eac8b3a0645c9dad9d1931c08
Asiak <asiakemp24@gmail.com>
2023-09-03 12:10:26 -0400

    started AboutMe.js

---
+++ react-portfolio/src/components/AboutMe.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+function AboutMe() {
+    return (
+      <section id="About-Me">
+        <div className="is-pulled-left">
+          <img src="./images/About-me.jpg" alt="About Me" />
+        </div>
+        <div className="content-section">
+          <div className="left-section">
+            <h2>About Me</h2>
+          </div>
+          <div className="right-section">
+            <p>
+              I am a graduate of Western Carolina University Class of 2019...
+            </p>
+          </div>
+        </div>
+      </section>
+    );
+  }
