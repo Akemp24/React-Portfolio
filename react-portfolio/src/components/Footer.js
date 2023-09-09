45dde3a0555a44542924ae6a8aeacbee789ffe2d
Asiak <asiakemp24@gmail.com>
2023-09-09 13:53:01 -0400

    simplified Footer

--- react-portfolio/src/components/Footer.js
+++ react-portfolio/src/components/Footer.js
@@ -1,19 +1,13 @@
-import React from "react";
+import React from 'react';
 
 function Footer() {
-    return (
-        <footer id="Contact-Info" className="content-section">
-            <div className="left-section">
-                <h2>Contact Info</h2>
-            </div>
-            <div className="right-section">
-                <ul>
-                    <li>Email: asiakemp24@gmail.com</li>
-                    <li>Phone: 704-310-7264</li>
-                </ul>
-            </div>
-        </footer>
-    );
+  return (
+    <footer className="footer">
+      <div className="copyright">
+        <p>&copy; 2023 Your Name</p>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
