886b09f565d89c5b7e5b2f196187664b6bb1bec0
Asiak <asiakemp24@gmail.com>
2023-09-09 15:30:52 -0400

    made changes to the resume section and some styling differences

--- react-portfolio/src/components/Footer.js
+++ react-portfolio/src/components/Footer.js
@@ -1,14 +1,39 @@
 import React from 'react';
 import "../styles/Footer.css";
+import { FaGithub, FaLinkedin } from 'react-icons/fa'; // Import icons
 
 function Footer() {
-    return (
-        <footer className="footer">
-            <div className="copyright">
-                <p>&copy; {new Date().getFullYear()} Your Name. All Rights Reserved.</p>
-            </div>
-        </footer>
-    );
+  return (
+    <footer>
+      <div className="footer-content">
+        <div className="footer-links">
+          <div className="icon-container">
+            <a
+              href="https://github.com/Akemp24"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="footer-link"
+            >
+              <FaGithub /> GitHub
+            </a>
+          </div>
+          <div className="icon-container">
+            <a
+              href="https://www.linkedin.com/in/asia-kemp-49a2041b2/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="footer-link"
+            >
+              <FaLinkedin /> LinkedIn
+            </a>
+          </div>
+        </div>
+        <div className="footer-text">
+          <p>&copy; {new Date().getFullYear()} Asia Kemp. All Rights Reserved</p>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
--- react-portfolio/src/components/Resume.js
+++ react-portfolio/src/components/Resume.js
@@ -1,18 +1,44 @@
 import React from 'react';
+import "../styles/Resume.css";
 
-const Resume = () => {
-  const resumeUrl = 'https://docs.google.com/document/d/1xvg3_iBbFLpcWJOdG5VloZHsBXIa5u49WZVhjwzgW9U/edit?usp=sharing';
+function Resume() {
   return (
-    <div>
-      <h2>Resume</h2>
-      <iframe
-        src={resumeUrl}
-        width="100%"
-        height="500px"
-        title="Resume"
-      ></iframe>
+    <div className="resume-container">
+      <h2 className="resume-heading">My Resume</h2>
+      <p>Download my resume:</p>
+      <a href="https://docs.google.com/document/d/e/2PACX-1vQKK4HFevqIkk9Pf3CYXh16TaLKGgUs8Vg0X4SMK6u7d2k6g_9UHjpwKiGz4fztCwWa7l_YJB2CIzAm/pub"
+       download="https://docs.google.com/document/d/e/2PACX-1vQKK4HFevqIkk9Pf3CYXh16TaLKGgUs8Vg0X4SMK6u7d2k6g_9UHjpwKiGz4fztCwWa7l_YJB2CIzAm/pub" className='resume-link'>
+        Download Resume
+      </a>
+
+      <h2 className="proficiencies-heading">Developer Proficiencies</h2>
+      <ul className="proficiencies-list">
+        <li>JavaScript</li>
+        <li>React.js</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Node.js</li>
+        <li>Express.js</li>
+        <li>SQL</li>
+        <li>MongoDB</li>
+        <li>NoSQL</li>
+        <li>GraphQL</li>
+        <li>jQuery</li>
+        <li>Bootstrap</li>
+        <li>Git</li>
+        <li>GitHub</li>
+        <li>Heroku</li>
+        <li>MERN Stack</li>
+        <li>RESTful APIs</li>
+        <li>Progressive Web Applications (PWAs)</li>
+        <li>Object Oriented Programming (OOP)</li>
+        <li>Test Driven Development (TDD)</li>
+        <li>Model View Controller (MVC)</li>
+        <li>Agile Methodologies</li>
+        <li>Responsive Design</li>
+      </ul>
     </div>
   );
-};
+}
 
 export default Resume;
